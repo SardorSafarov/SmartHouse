2bfed10a55eb27c46f33003ca334b0dc0decdee5
SardorSafarov <zomin000s@gmail.com>
2022-09-20 22:02:51 +0500

    alert dialog done

--- app/src/main/java/com/example/zamin/smarthouse/MainActivity.kt
+++ app/src/main/java/com/example/zamin/smarthouse/MainActivity.kt
@@ -1,15 +1,35 @@
 package com.example.zamin.smarthouse
 
+import android.content.DialogInterface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+import com.example.zamin.smarthouse.databinding.ActivityMainBinding
+import com.example.zamin.smarthouse.databinding.DialogChangePhoneNumberBinding
 import com.example.zamin.smarthouse.utils.statusBarColor
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         statusBarColor(window,"#3daafc")
+        changePhoneNumber()
+
+    }
+
+    private fun changePhoneNumber() {
+        binding.changePhoneNumber.setOnClickListener {
+            val alertDialog = AlertDialog.Builder(this,R.style.CustomAlertDialog)
+
+            val view = LayoutInflater.from(this).inflate(R.layout.dialog_change_phone_number,null)
+            val dialogView = DialogChangePhoneNumberBinding.bind(view)
+            alertDialog.setView(view)
 
 
+            alertDialog.show()
+        }
     }
 }
