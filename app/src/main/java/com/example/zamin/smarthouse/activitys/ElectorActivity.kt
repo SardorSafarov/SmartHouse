3b8afd22075cc2e28e512ee12ab8c22e9e50c04b
SardorSafarov <zomin000s@gmail.com>
2022-09-21 09:31:57 +0500

    items done

--- app/src/main/java/com/example/zamin/smarthouse/activitys/ElectorActivity.kt
+++ app/src/main/java/com/example/zamin/smarthouse/activitys/ElectorActivity.kt
@@ -2,7 +2,10 @@ package com.example.zamin.smarthouse.activitys
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.CompoundButton
+import android.widget.Toast
 import com.example.zamin.smarthouse.R
+import com.example.zamin.smarthouse.app.d
 import com.example.zamin.smarthouse.app.statusBarColor
 import com.example.zamin.smarthouse.databinding.ActivityElectorBinding
 
@@ -12,6 +15,21 @@ class ElectorActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityElectorBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        statusBarColor(window,getString(R.string.statusbar_color))
+        elctorOnOff()
+    }
+
+    private fun elctorOnOff() {
+        binding.switchElector.setOnCheckedChangeListener({ compoundButton: CompoundButton, b: Boolean ->
+            when(b){
+                true->{
+                    Toast.makeText(this, "yondi", Toast.LENGTH_SHORT).show()
+                }
+                else->{
+                    Toast.makeText(this, "O`chdi", Toast.LENGTH_SHORT).show()
+                }
+            }
+        })
+
+        
     }
 }
